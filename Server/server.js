cd9b3453c04be3c27f42d1f7c561326434a13bf7
ADITYA <adityapuri85@gmail.com>
2022-12-26 15:27:04 +0530

    done

---
+++ Server/server.js
@@ -0,0 +1,61 @@
+import express from "express";
+
+import * as dotenv from "dotenv";
+
+import cors from "cors";
+
+import { Configuration, OpenAIApi } from "openai";
+//because we have use import not require for importing module so add type:module in package json..
+// for starting server add server:"nodemon _File_NAME_" in script
+
+dotenv.config();
+
+console.log(process.env.OPENAI_API_KEY);
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+const app = express();
+
+app.use(cors()); //for cors err
+
+app.use(express.json()); //used for json req
+
+app.get("/", async (req, res) => {
+  res.status(200).send({
+    message: "HelloADY",
+  });
+});
+
+app.post("/", async (req, res) => {
+  try {
+    //name=prompt in textarea
+    const prompt = req.body.prompt;
+
+    const response = await openai.createCompletion({
+      //read docs
+      model: "text-davinci-003",
+      prompt: `${prompt}`, //passes users prompt
+
+      temperature: 0, //high temp means model will take more risk so temp:0
+      max_tokens: 3000, //no of tokens to generate after completion
+      top_p: 1,
+      frequency_penalty: 0.5, //freq penality means that it not going to repeate similar sentence...
+      presence_penalty: 0,
+      // stop: ['"""'], //we dont need stop
+    });
+    res.status(200).send({
+      bot: response.data.choices[0].text,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send({ err });
+  }
+});
+
+//server to listen always
+app.listen(5000, () => {
+  console.log("Server up on port 5000");
+});
---
+++ client/main.js
@@ -0,0 +1,91 @@
+import bot from "./assets/bot.svg";
+import user from "./assets/user.svg";
+
+//targeting element..
+const form = document.querySelector("form");
+const chatContainer = document.querySelector("#chat_container");
+
+console.log(form);
+
+let loadInterval;
+
+//loader while CodeY is thinking...
+function loader(element) {
+  element.textContent = "";
+
+  //here below i added a 3 dots loading at interval of 300ms...
+  loadInterval = setInterval(() => {
+    element.textContent += ".";
+
+    if (element.textContent === "....") {
+      element.textContent = "";
+    }
+  }, 300);
+}
+
+//letter by letter typing function..
+function typeText(element, text) {
+  let index = 0;
+  let inteval = setInterval(() => {
+    if (index < text.length) {
+      //it means we are still typing
+      element.innerHTML += text.charAt(index);
+      index++;
+    } else {
+      clearInterval(inteval);
+    }
+  }, 20);
+}
+
+function generateUniqueId() {
+  //alway use current time and a random functions
+  const timeStamp = Date.now();
+  const randomNum = Math.random();
+  const hexadecimalString = randomNum.toString(16);
+  return `id-${timeStamp}-${hexadecimalString} `;
+}
+
+function chatStripe(isAI, value, uniqueId) {
+  return `
+    <div class="wrapper ${isAI && "ai"}">
+      <div class="chat">
+        <div class="profile>
+          <img src="${isAI ? bot : user}" alt="${isAI ? "bot" : "user"}"/>
+        </div>
+        <div class="message" id=${uniqueId}>${value}</div>
+      </div>
+    </div> 
+    `;
+}
+
+const handleSubmit = async (e) => {
+  e.preventDefault();
+  const data = new FormData(form);
+  console.log(data);
+  //user chat-stripe
+  chatContainer.innerHTML += chatStripe(false, data.get("prompt"));
+
+  form.reset(); //resseting form
+
+  //bots chatStrip...
+  const uniqueId = generateUniqueId();
+  console.log(uniqueId);
+  chatContainer.innerHTML += chatStripe(true, " ", uniqueId);
+  console.log(chatContainer);
+  //as we get typed message we want to scroll down...
+  chatContainer.scrollTop = chatContainer.scrollHeight;
+
+  //err...below
+  // const messageDiv = document.querySelector(`${uniqueId}`);
+  console.log(messageDiv);
+  loader(messageDiv);
+};
+
+form.addEventListener("submit", handleSubmit);
+
+form.addEventListener("keyup", (e) => {
+  if (e.keyCode === 13) {
+    //code for enter key
+    handleSubmit(e);
+  }
+});
